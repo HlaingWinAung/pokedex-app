3ca8f07e50f42545a51aec578ad2797ff2382d7d
HlaingWinAung <hlaingwinaung76gmail.com>
2021-12-14 15:56:50 +0630

    Updates

---
+++ precache-manifest.d0feba5954f35a5472e1ce53a823fdb1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6f526e34490b798406d",
+    "url": "/pokedex-app/static/css/main.cc19a75e.chunk.css"
+  },
+  {
+    "revision": "b6f526e34490b798406d",
+    "url": "/pokedex-app/static/js/main.b6f526e3.chunk.js"
+  },
+  {
+    "revision": "ad961163a52a17acddf4",
+    "url": "/pokedex-app/static/js/1.ad961163.chunk.js"
+  },
+  {
+    "revision": "e176a0d26b312e7b361a",
+    "url": "/pokedex-app/static/js/runtime~main.e176a0d2.js"
+  },
+  {
+    "revision": "fc5bc6d277bafa1ec5e2b3717e835c0f",
+    "url": "/pokedex-app/index.html"
+  }
+];
